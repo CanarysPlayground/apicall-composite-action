067cc30b750f06be0781a2bc1bcb4e9a05155b90
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2021-11-30 05:53:38 +0530

    Update apicalltest.ps1

--- apicalltest.ps1
+++ apicalltest.ps1
@@ -9,7 +9,11 @@ param(
 
 #$Header = "application/vnd.github.v3+json"
 
+# Sample Request Body
+
 #$RequestBody = '{"name":"Created-From-Script,"visibility":"private"}'
+
+#Define your header below
 
 $AuthorizationHeader = @{
     Authorization = 'Bearer ' + $token
@@ -20,7 +24,7 @@ $repobody = $RequestBody
 
 
  $getapirequest = @{
-		Uri = "https://api.github.com/orgs/CanarysPlayground/repos"
+		Uri = "https://api.github.com/orgs/CanarysPlayground/repos" #add your api url here
 		Method = "Post"
                 body = $repobody
 		ContentType = "application/json"
@@ -30,6 +34,8 @@ $repobody = $RequestBody
 $apiobject = Invoke-RestMethod @getapirequest -TimeoutSec 5000
 
 $output = $apiobject.name
+
+# Expected Output comparision with the generated output
 
 if ($output -ne $ExpectedOutput)
 {
